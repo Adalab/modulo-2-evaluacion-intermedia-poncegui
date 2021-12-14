49e2b2687def3bbcb43cce9d73178d8cb5fee0c6
poncegui <marta.ponceg@gmail.com>
2021-12-14 10:12:26 +0100

    randomNumber

--- js/main.js
+++ js/main.js
@@ -8,13 +8,30 @@ const btn = document.querySelector(".js_button");
 //Get random number from 1 to 10
 
 const numberResult = getRandomNumber (10); 
-const form = document.querySelector (".js-form"); 
+// const form = document.querySelector (".js-form"); 
+let stone =document.querySelector (".js-stone"); 
+let scissors =document.querySelector (".js-paper"); 
+let paper = document.querySelector (".js-scissors"); 
+let scoreGamePhrase = document.querySelector (".js-result"); 
 
 function getRandomNumber(max) {
 return Math.ceil(Math.random() * max); 
 }
 console.log(numberResult);
 
+// Asignamos valor de piedra papel tijera al número random
+function randomChose(){
+    const numberResult = face.value;
+    if (numberResult <= 4){
+        numberResult = stone;
+    }else if (numberResult > 4 || numberResult <= 6){
+         numberResult = paper;
+        }else if (numberResult > 7){
+            numberResult = scissors;
+       
+    }
+
+}
 // Function game 
 
 // ********************************************************************
@@ -26,6 +43,8 @@ console.log(numberResult);
 
 function handleBtnClick (){
     getRandomNumber();
+    randomChose();
+    Game();
 
 }
 btn.addEventListener("click", handleBtnClick);
