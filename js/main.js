f19497b3bb6f2a6a1ede024beb966597b8adbd22
poncegui <marta.ponceg@gmail.com>
2021-12-18 20:16:44 +0100

    dev updated js

--- js/main.js
+++ js/main.js
@@ -1,86 +1,254 @@
 'use strict';
 
-// Declaramos las variables
-const btn = document.querySelector(".js_button"); 
+// ************************************************************************************
+// HTML elements
+// ************************************************************************************
+
+let btn = document.querySelector(".js_button"); 
 let scorePlayer = document.querySelector(".js-player"); 
 let scoreComputer = document.querySelector(".js-computer");
 let result = document.querySelector(".js_result");
+let changeBkg = document.querySelector(".body");
+const resetBtn = document.querySelector(".hiddenResetBtn")
+const rockIconP = document.querySelector(".iconHiddenRp");
+const paperIconP = document.querySelector(".iconHiddenPp");
+const scissorsIconP = document.querySelector(".iconHiddenSp");
+const rockIconC = document.querySelector(".iconHiddenRc");
+const paperIconC = document.querySelector(".iconHiddenPc");
+const scissorsIconC = document.querySelector(".iconHiddenSc");
 
-//Get random number from 1 to 10
+// ************************************************************************************
+// global elements
+// ************************************************************************************
 
-
-// const form = document.querySelector (".js-form"); 
 let pcChose ="";
+let playerPoints = 0;
+let pcPoints = 0;
+let attemps = 0;
+
+
+
+// ************************************************************************************
+//Get random number from 1 to 10 & Asign value "piedra papel tijera" to random number
+// ************************************************************************************
+
+
 function getRandomNumber(max) {
 return Math.ceil(Math.random() * max); 
 }
 
-// Asignamos valor de piedra papel tijera al número random
 
 function generatePcChose() {
     pcChose = getRandomNumber(10);
-    if (pcChose >= 0 && pcChose < 3) {
+    if (pcChose >= 0 && pcChose <= 3) {
         pcChose = "piedra";
-    } else if (pcChose >= 3 && pcChose < 6) {
+    } else if (pcChose > 3 && pcChose < 6) {
         pcChose = "papel";
     } else if (pcChose >= 6 && pcChose <= 10) {
       pcChose = "tijera";
     }
-    console.log(pcChose);
-  }
+}
 
-let playerPoints = 0;
-let pcPoints = 0;
-  
 
-  // Function game; 
+// ************************************************************************************
+// Function game
+// ************************************************************************************
+
+// this function is for exchange main message as per game results
+
 function writeResult  (message) {
     result.innerHTML = message;
-  }
+}
 
 console.log(chose.value);
 function game(){
     if (chose.value === pcChose){
         writeResult("Empate");
+        attemps ++;
     } else if ((chose.value === "papel") && (pcChose === "piedra")) {
-        writeResult("Has ganado!");
-       playerPoints ++;
-    } else if ((chose.value === "tijera") && (pcChose === "papel")) {
-        writeResult("Has ganado!");
+        writeResult("!Has ganado!");
         playerPoints ++;
+        attemps ++;
+
+    } else if ((chose.value === "tijera") && (pcChose === "papel")) {
+        writeResult("!Has ganado!");
+        playerPoints ++;
+        attemps ++;
 
     } else if ((chose.value === "piedra") && (pcChose === "tijera")) {
-        writeResult("Has ganado!");
+        writeResult("!Has ganado!");
         playerPoints ++;
+        attemps ++;
+
+    } else if ((chose.value === "papel") && (pcChose === "tijera")) {
+        writeResult("!Has ganado!");
+        playerPoints ++;
+        attemps ++;
+
     } else {
-        writeResult("Has perdido!");
+        writeResult("!Has perdido!");
         pcPoints ++;
+        attemps ++;     
     }
+
     scoreComputer.innerHTML = "Ordenador: " + pcPoints;
     scorePlayer.innerHTML = "Jugador:  " + playerPoints;
 }
 
-// ********************************************************************
-
-// Function score; según el resultado vamos a un marcador u otro 
-
-// function score(){
-//     if (writeResult ("Has ganado!");
-
-// }
 
 
+// ************************************************************************************
+// Shows icons as per game result (not requered on exercise)
+// ************************************************************************************
 
 
-//Eventos de "btn click"
+function iconsPlayer(){
+    if (chose.value === "tijera"){
+        scissorsIconP.classList.remove("iconHiddenSp");
+        paperIconP.classList.add("iconHiddenPp");
+        rockIconP.classList.add("iconHiddenRp");
+
+
+    }else if (chose.value === "piedra"){
+        rockIconP.classList.remove("iconHiddenRp");
+        paperIconP.classList.add("iconHiddenPp");
+        scissorsIconP.classList.add("iconHiddenSp");
+
+
+
+    } else if (chose.value === "papel"){
+        paperIconP.classList.remove("iconHiddenPp");
+        rockIconP.classList.add("iconHiddenRp");
+        scissorsIconP.classList.add("iconHiddenSp");
+      }
+  }
+  
+  function iconsComputer(){
+    if (pcChose === "tijera"){
+        scissorsIconC.classList.remove("iconHiddenSc");
+        paperIconC.classList.add("iconHiddenPc");
+        rockIconC.classList.add("iconHiddenRc");
+
+
+    }else if (pcChose === "piedra"){
+        rockIconC.classList.remove("iconHiddenRc");
+        paperIconC.classList.add("iconHiddenPc");
+        scissorsIconC.classList.add("iconHiddenSc");
+
+
+
+    } else if (pcChose === "papel"){
+        paperIconC.classList.remove("iconHiddenPc");
+        rockIconC.classList.add("iconHiddenRc");
+        scissorsIconC.classList.add("iconHiddenSc");
+    }
+}
+
+
+function hideIconsReset(){
+    if (attemps === 10){
+        paperIconC.classList.add("iconHiddenPc");
+        rockIconC.classList.add("iconHiddenRc");
+        scissorsIconC.classList.add("iconHiddenSc");
+        paperIconP.classList.add("iconHiddenPp");
+        rockIconP.classList.add("iconHiddenRp");
+        scissorsIconP.classList.add("iconHiddenSp");
+
+    }
+    
+}    
+
+// ************************************************************************************
+// Counter resets when it reaches 10 attemps and select the winner // also handle btns
+// ************************************************************************************
+
+function counterResets() {
+    
+    if ((attemps === 10) && (pcPoints < playerPoints)) {
+        pcPoints = 0; playerPoints = 0;
+        writeResult("Partida finalizada, ¡Has ganado!");
+        btn.classList.add("hiddenResetBtn");
+        resetBtn.classList.add("resetBtn");
+        changeBkg.classList.add("changeBkgWinner");
+        trophy.classList.remove("js-imgHidden");
+        trophy.classList.add("img");
+      
+
+      }  else if ((attemps === 10) && (pcPoints > playerPoints)){
+        pcPoints = 0; playerPoints = 0;
+        writeResult("Partida finalizada, ¡Has perdido!");
+        changeBkg.classList.add("changeBkgDark");
+        btn.classList.add("hiddenResetBtn");
+        resetBtn.classList.add("resetBtn");
+        mario.classList.remove("js-marioHidden");
+        mario.classList.add("mario");
+
+
+    }  else if ((attemps === 10) && (pcPoints === playerPoints)){
+        pcPoints = 0; playerPoints = 0;
+        writeResult("Partida finalizada, ¡Empate!");
+        btn.classList.add("hiddenResetBtn");
+        resetBtn.classList.add("resetBtn");
+     
+         
+    } 
+}
+
+
+// ************************************************************************************
+//  Funtion to add and remove btns also change background color as per result
+// ************************************************************************************
+
+function resetGame() {
+    btn.classList.remove("hiddenResetBtn");
+    resetBtn.classList.remove("resetBtn");
+    playerPoints = 0;
+    pcPoints = 0;
+    scorePlayer.innerHTML = "Jugador:  " + 0;
+    scoreComputer.innerHTML ="Ordenador: " + 0;
+    writeResult("¡Vamos a jugar!");
+    changeBkg.classList.remove("changeBkgWinner");
+    changeBkg.classList.remove("changeBkgDark");
+    hideIconsReset();
+    mario.classList.add("js-marioHidden");
+    trophy.classList.add("js-imgHidden");
+    
+
+}
+// ************************************************************************************
+// gif not included on exercise
+// ************************************************************************************
+   const mario = document.querySelector(".js-marioHidden");
+   const trophy = document.querySelector(".js-imgHidden");
+
+   
+
+// ************************************************************************************
+// Events
+// ************************************************************************************
 
 function handleBtnClick (ev){
     ev.preventDefault();
     generatePcChose();
-    game()
- 
+    game();
+    counterResets();
+    iconsPlayer();
+    iconsComputer();
 
+ 
+}
+
+function handleResetBtnClick(ev) {
+    ev.preventDefault();
+    resetGame();
+    hideIconsReset();
+    gifHidden();
 
 }
 
+// ************************************************************************************
+// Listeners
+// ************************************************************************************
+
 btn.addEventListener("click", handleBtnClick);
+resetBtn.addEventListener("click", handleResetBtnClick);
